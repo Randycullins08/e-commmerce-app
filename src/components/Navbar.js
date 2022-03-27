d69b560f73fbf218bb8237d9abad861e988bdd76
Randy Cullins <randycullins2018@gmail.com>
2022-03-27 06:46:57 -0600

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,20 @@ export default function Navbar() {
   return (
     <div className="navbar-container">
       <div className="logo-wrapper">
-        <h1>Randy Cullins</h1>
+        <h1>
+          <span>Randy Cullins</span>
+        </h1>
+      </div>
+
+      <div className="links-wrapper">
+        <button>Home</button>
+        <button>Shop</button>
+        <button>Contact</button>
+        <button>About</button>
+      </div>
+
+      <div className="user-wrapper">
+        <h3>User</h3>
       </div>
     </div>
   );
