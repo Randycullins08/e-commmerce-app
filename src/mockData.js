6e5e18a99d0d67a98a65d05b1f67bbaee36c59f3
Randy Cullins <randycullins2018@gmail.com>
2022-03-27 07:23:35 -0600

    mockData started

---
+++ src/mockData.js
@@ -0,0 +1,106 @@
+const items = [
+  {
+    id: 1,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 2,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 3,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 4,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 5,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 6,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 7,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 8,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 9,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 10,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 11,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 12,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 13,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 14,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+
+  {
+    id: 15,
+    image: "some image",
+    name: "product name",
+    description: "product description",
+  },
+];
