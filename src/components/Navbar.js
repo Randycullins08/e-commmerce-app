67214a2c3eaea2f238aa7c0f50ff4b646576b877
Randy Cullins <randycullins2018@gmail.com>
2022-03-27 06:10:18 -0600

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="logo-wrapper">
+        <h1>Randy Cullins</h1>
+      </div>
+    </div>
+  );
+}
