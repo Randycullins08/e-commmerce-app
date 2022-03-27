3d2bc29f8a41f5ad98dada863da759cad36735d3
Randy Cullins <randycullins2018@gmail.com>
2022-03-27 07:16:28 -0600

    footer

---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+export default function Footer() {
+  return (
+    <div className="footer-container">
+      <div className="nav-links-wrapper">
+        <button>Home</button>
+        <button>Shop</button>
+        <button>Contact</button>
+        <button>About</button>
+      </div>
+
+      <div className="copyright-container">
+        <p>Copyright Randy Cullins</p>
+      </div>
+    </div>
+  );
+}
